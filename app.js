a0942cba6a0d9ba0fd8176d91212629dde313796
guslx <gusinj@outlook.com>
2022-12-22 17:33:20 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const form = document.querySelector('.quiz-form')
+const finalResult = document.querySelector('.result')
+
+const correctAnswers = ['A', 'A', 'B', 'A']
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const valueOfFirstQuestion = form.inputQuestion1.value 
+    const valueOfSecondQuestion = form.inputQuestion2.value 
+    const valueOfThirdQuestion = form.inputQuestion3.value 
+    const valueOfFourthQuestion = form.inputQuestion4.value
+
+    let score = 0
+
+    const userAnswers = [
+        valueOfFirstQuestion,
+        valueOfSecondQuestion,
+        valueOfThirdQuestion,
+        valueOfFourthQuestion
+    ]
+
+
+    userAnswers.forEach((userAnswer, index) => {
+        const userAnsweredCorrectly = userAnswer === correctAnswers[index]
+        if(userAnsweredCorrectly) {
+            score += 25
+        }
+    })
+
+    
+    scrollTo(0, 0)
+
+    finalResult.style.display= 'block'
+
+
+    let counter = 0
+
+    const timer = setInterval(() => {
+        if(counter === score) {
+            clearInterval(timer)
+        }
+
+        finalResult.querySelector('span').textContent = `${counter}%`
+        counter++
+    }, 10)
+})
